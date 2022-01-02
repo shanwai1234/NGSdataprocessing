3f402ee85c7fab311ce1cb53f363048611292590
Zhikai <zliang@huskers.unl.edu>
2022-01-02 15:19:27 -0600

    Update df.R

--- ATACseq/df.R
+++ ATACseq/df.R
@@ -2,9 +2,9 @@ library(DiffBind)
 x = read.table("DiffBind-B73-Cold.txt",sep='\t',head=T)
 tamoxifen <- dba(sampleSheet=x)
 tamoxifen <- dba.count(tamoxifen)
+# summit determination
+# tamoxifen.DB <- dba.count(tamoxifen2, summits=TRUE, score=DBA_SCORE_SUMMITS_POS)
 tamoxifen1 <- dba.contrast(tamoxifen, categories=DBA_CONDITION, minMembers=2)
 tamoxifen2 <- dba.analyze(tamoxifen1)
 tamoxifen.DB <- dba.report(tamoxifen2)
-# summit determination
-# tamoxifen.DB <- dba.count(tamoxifen2, summits=TRUE, score=DBA_SCORE_SUMMITS_POS)
 write.table(tamoxifen.DB, file="test.txt")
